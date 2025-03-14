2cf6947a9726e6a3487953c82769504d9ccbf8b6
GreyTown <aggreykanyoro@gmail.com>
2025-03-14 13:22:28 +0300

    working on the nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,37 @@
+import React from 'react';
+
+
 export function Header() {
     return (
-      <header className="w-full p-6 bg-gradient-to-r from-teal-500 to-blue-600 text-white text-center shadow-lg">
-        <h1 className="text-4xl font-extrabold tracking-tight">Crypto Cookie Jar</h1>
-        <p className="text-sm mt-1 opacity-80">Baked by Abwooli, Vibin’ with Amooti</p>
-      </header>
+      <nav className="w-full flex space-x-2 p-6 bg-gradient-to-r from-teal-500 to-blue-600 text-white sm:space-x-2 shadow-lg">
+        <div className="container mx-auto px-4 py-2 sm:px-6 lg:px-8 ">
+          <div className="flex items-center justify-between h-8"> 
+                
+                {/* Logo */}
+            <div className="logo">
+            <h1 className="text-2xl font-extrabold tracking-tight">Decentra-DAO</h1>
+            <h4 className="font-bold">TreasuryApp</h4>
+            <p className="text-sm mt-1 opacity-80">Baked by Abwooli, Vibin’ with Amooti</p>
+            </div>
+
+                {/* Desktop Menu */}
+            <div className="hidden md:flex gap-2 space-x-4">
+              <a href="#" className="hover:text-amber-500">Home</a>
+              <a href="#" className="hover:text-amber-500">About Us</a>
+              <a href="#" className="hover:text-amber-500">Contact</a>
+              <a href="#" className="hover:text-amber-500">Members</a>
+            </div>
+
+                {/* Auth btn */} 
+            <div className='display hover:cursor-pointer flex gap-2'>
+              <h2>Login</h2>
+              <button className="bg-amber-700 hover:cursor-pointer ">Connect <br/> Wallet</button>
+            </div>
+                
+                {/* Mobile Menu */}
+                <div className="md:hidden">Mobile menu</div>
+          </div>
+        </div>
+      </nav>
     );
-  }
+};
